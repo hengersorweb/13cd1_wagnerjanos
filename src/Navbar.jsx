417a9170b6f9cbe6399543267172bc52584b3f6f
wagnerjanos18 <wagjan151@hengersor.hu>
2025-02-17 11:58:26 +0100

    Router beállítás
    Menü rendszer kialakítása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
+import {Link, NavLink} from "react-router-dom"
+
 
 const Navbar = (()=>{
 
         return(
             <div>
-                <h1>Menü</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae atque dignissimos possimus odit, nemo, ab explicabo dolores adipisci aut, praesentium reiciendis vero exercitationem laborum! Adipisci aspernatur eveniet doloremque? Nisi cum molestias ipsa voluptatibus iure aliquam sapiente harum, magnam beatae optio accusamus quidem iusto amet, ipsum repellat ratione rem animi reprehenderit itaque error atque. Delectus repellendus nobis quasi quis, placeat nisi at perspiciatis magni optio expedita enim quae culpa accusamus hic dignissimos saepe totam quidem cumque ut sint dolorem possimus cupiditate omnis temporibus? Cum delectus quibusdam soluta? Velit blanditiis dolores magnam illum quasi, aut ipsam reprehenderit, error totam atque obcaecati temporibus.</p>
+
+                <nav>
+                    <Link to='/'>Főoldal</Link>
+                    <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
+                    <NavLink to='/mermesek'>Az olimpia magyar érmesei</NavLink>
+                </nav>
+               
             </div>
         )
 }
