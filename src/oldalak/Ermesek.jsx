672f564fa7387834a807ee8f9ad28e82b3436a73
wagnerjanos18 <wagjan151@hengersor.hu>
2025-02-17 11:29:22 +0100

    szükséges modulok import
    bootstrap , router, tachyons

---
+++ src/oldalak/Ermesek.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ermesek = (()=>{
+
+        return(
+            <div>
+                <h1>Az olimpia érmesei</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae atque dignissimos possimus odit, nemo, ab explicabo dolores adipisci aut, praesentium reiciendis vero exercitationem laborum! Adipisci aspernatur eveniet doloremque? Nisi cum molestias ipsa voluptatibus iure aliquam sapiente harum, magnam beatae optio accusamus quidem iusto amet, ipsum repellat ratione rem animi reprehenderit itaque error atque. Delectus repellendus nobis quasi quis, placeat nisi at perspiciatis magni optio expedita enim quae culpa accusamus hic dignissimos saepe totam quidem cumque ut sint dolorem possimus cupiditate omnis temporibus? Cum delectus quibusdam soluta? Velit blanditiis dolores magnam illum quasi, aut ipsam reprehenderit, error totam atque obcaecati temporibus.</p>
+            </div>
+        )
+}
+)
+
+export default Ermesek
