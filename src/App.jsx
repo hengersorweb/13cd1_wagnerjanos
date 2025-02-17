03cd162ad9776762206ed16b0c3722b930928678
wagnerjanos18 <wagjan151@hengersor.hu>
2025-02-17 11:43:27 +0100

    router beállítása

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,11 @@ function App() {
     <>
       <div>
       <h1>Covid</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/ermesek" element={<Ermesek/>} />
+        <Route path="/mermesek" element={<Mermesek/>} />
+      </Routes>
       </div>
     </>
   )
